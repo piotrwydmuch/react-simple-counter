f1f8c3e927e38a89823ca2cbdb6ef26add6b11a0
Piotr <piotrekwyd@gmail.com>
2020-04-09 22:25:02 +0200

    Updates

---
+++ precache-manifest.5d5b68893f886461229c85beb912028c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4faa136a36ba7834acd3300389e05a7e",
+    "url": "/react-simple-counter/index.html"
+  },
+  {
+    "revision": "a30434bc29677debf9dd",
+    "url": "/react-simple-counter/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "73354bbed44c3ed16c50",
+    "url": "/react-simple-counter/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a30434bc29677debf9dd",
+    "url": "/react-simple-counter/static/js/2.1831f105.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-simple-counter/static/js/2.1831f105.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73354bbed44c3ed16c50",
+    "url": "/react-simple-counter/static/js/main.e3a45391.chunk.js"
+  },
+  {
+    "revision": "f9818ee22ab37853a55b",
+    "url": "/react-simple-counter/static/js/runtime-main.2cc0f28f.js"
+  }
+]);
